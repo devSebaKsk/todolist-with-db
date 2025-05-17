64f88cae2b4230e8b2bed15d6af579fe519e5c5d
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-05-17 23:21:25 +0000

    Feature: Creacion de Db de las notas

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
@@ -16,4 +17,20 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Note(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(120), nullable=False)
+    body: Mapped[str] = mapped_column(String(240), nullable=False)
+    status: Mapped[bool] = mapped_column(Boolean(),nullable=False)
+    create_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc), nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "title":self.title,
+            "body":self.body,
+            "status":self.status,
+            "create_at":self.create_at
         }
