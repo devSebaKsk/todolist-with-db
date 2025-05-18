c083689dabc20a4eaefa079667ca7951d929faa5
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-05-18 01:24:03 +0000

    Feature: end point Post nota y get Listado notas

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Note
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -20,3 +20,24 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/notes/int:id', methods=['GET'])
+def handle_get_note(id):
+    note = Note.query.get(id)
+    if not note:
+        return jsonify({"msg": "Empresa no encontrada"}), 404
+
+    return jsonify(note), 200
+
+
+@api.route('/notes', methods=['POST'])
+def handle_post_note():
+    data = request.get_json(silent=True)
+    note = Note(
+        title=data.get("title"),
+        body=data.get("body")
+        )
+    db.session.add(note)
+    db.session.commit()
+    return jsonify({"message": "Nota creada"}), 201
+
