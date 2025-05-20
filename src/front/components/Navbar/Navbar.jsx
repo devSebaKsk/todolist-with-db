127df82f2cf700de56bf55d6d5fe3861831c3a0c
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-05-20 03:10:42 +0000

    Feature: Visual navbar

---
+++ src/front/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.scss";
+
+export const Navbar = () => {
+
+	return (
+		<nav className="navbar">
+				<span>My Notes</span>
+				<div>
+					<button><i class="fa-solid fa-user"></i></button>
+				</div>
+		</nav>
+	);
+};
