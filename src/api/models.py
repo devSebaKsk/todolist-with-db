3c3e07dedf0a7b0459b492dfcc408c2e7a41eb7d
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-05-27 20:20:39 +0000

    Db user camps creation

--- src/api/models.py
+++ src/api/models.py
@@ -8,6 +8,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
@@ -16,6 +17,7 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
             # do not serialize the password, its a security breach
         }
     
